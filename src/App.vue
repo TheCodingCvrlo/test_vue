46e7455981f92bde8b95d0e0d715cd16f5925c4a
Carlo <carlopatti1@gmail.com>
2022-12-27 17:54:38 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UsersPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UsersPage from './components/UsersPage.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    UsersPage
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/UsersPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h3> Users:</h3>
+  </template>
+  
+  <script>
+    export default {
+      name: 'UsersPage',
+    }
+  </script>
+  
+  <style>
+    h3 {
+      margin-bottom: 5%;
+    }
+  </style>
